618f67a8a872981e022f746f14d49c74f30f88da
shmon <shmonyajik@yandex.ru>
2023-12-22 08:01:35 +0300

    Some files renamed

--- lab12.cpp
+++ lab12.cpp
@@ -7,20 +7,27 @@
 #define MATRIX_SIZE 5
 
 int main(int argc, char* argv[]) {
+    // Инициализация MPI
     MPI_Init(NULL, NULL);
 
+    // Получение ранга текущего процесса и общего числа процессов
     int processRank, totalProcesses;
     MPI_Comm_rank(MPI_COMM_WORLD, &processRank);
     MPI_Comm_size(MPI_COMM_WORLD, &totalProcesses);
 
+    // Вычисление количества строк, обрабатываемых каждым процессом
     int rowsPerProcess = MATRIX_SIZE / totalProcesses;
+
+    // Создание векторов для матриц и результатов
     std::vector<int> matrixA(MATRIX_SIZE * MATRIX_SIZE);
     std::vector<int> matrixB(MATRIX_SIZE * MATRIX_SIZE);
     std::vector<int> matrixC(MATRIX_SIZE * MATRIX_SIZE);
 
+    // Создание векторов-буферов для данных
     std::vector<int> bufferB(MATRIX_SIZE * rowsPerProcess);
     std::vector<int> partialResult(MATRIX_SIZE * rowsPerProcess);
 
+    // Измерение времени выполнения
     double startTime, stopTime;
 
     if (processRank == 0) {
@@ -48,7 +55,14 @@ int main(int argc, char* argv[]) {
 
         // Распределение данных матрицы B
         for (int i = 1; i < totalProcesses; i++) {
-            MPI_Send(matrixB.data() + i * rowsPerProcess * MATRIX_SIZE, MATRIX_SIZE * rowsPerProcess, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(
+                matrixB.data() + i * rowsPerProcess * MATRIX_SIZE,   // Указатель на начало блока данных для отправки
+                MATRIX_SIZE * rowsPerProcess,                       // Количество элементов в блоке данных
+                MPI_INT,                                            // Тип отправляемых элементов (в данном случае, целые числа)
+                i,                                                  // Ранг процесса-получателя
+                0,                                                  // Тег сообщения - идентификатор сообщения
+                MPI_COMM_WORLD                                      // Коммуникатор, который определяет группу процессов, среди которых происходит обмен
+            );
         }
 
         // Распределение данных матрицы A
@@ -69,13 +83,15 @@ int main(int argc, char* argv[]) {
 
         // Сбор результатов от других процессов
         for (int processIndex = 1; processIndex < totalProcesses; processIndex++) {
-            MPI_Recv(partialResult.data(), MATRIX_SIZE * rowsPerProcess, MPI_INT, processIndex, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-            for (int i = 0; i < rowsPerProcess; i++) {
-                for (int j = 0; j < MATRIX_SIZE; j++) {
-                    matrixC[(processIndex * rowsPerProcess + i) * MATRIX_SIZE + j] = partialResult[i * MATRIX_SIZE + j];
-                }
-            }
+            MPI_Recv(
+                partialResult.data(),                               // Указатель на буфер для принятия данных
+                MATRIX_SIZE * rowsPerProcess,                       // Количество элементов, которые ожидаются в блоке данных
+                MPI_INT,                                            // Тип принимаемых элементов (в данном случае, целые числа)
+                processIndex,                                       // Ранг процесса-отправителя
+                3,                                                  // Тег сообщения - идентификатор сообщения
+                MPI_COMM_WORLD,                                     // Коммуникатор, который определяет группу процессов, среди которых происходит обмен
+                MPI_STATUS_IGNORE                                   // Статус сообщения (в данном случае, игнорируется)
+            );
         }
 
         stopTime = MPI_Wtime();
@@ -95,6 +111,14 @@ int main(int argc, char* argv[]) {
     else {
         // Получение данных матрицы B
         MPI_Recv(bufferB.data(), MATRIX_SIZE * rowsPerProcess, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Send(
+                matrixB.data() + i * rowsPerProcess * MATRIX_SIZE,   // Указатель на начало блока данных для отправки
+                MATRIX_SIZE * rowsPerProcess,                       // Количество элементов в блоке данных
+                MPI_INT,                                            // Тип отправляемых элементов (в данном случае, целые числа)
+                i,                                                  // Ранг процесса-получателя
+                0,                                                  // Тег сообщения - идентификатор сообщения
+                MPI_COMM_WORLD                                      // Коммуникатор, который определяет группу процессов, среди которых происходит обмен
+            );
 
         // Получение данных матрицы A
         MPI_Recv(matrixA.data(), MATRIX_SIZE * rowsPerProcess, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -114,6 +138,7 @@ int main(int argc, char* argv[]) {
         MPI_Send(partialResult.data(), MATRIX_SIZE * rowsPerProcess, MPI_INT, 0, 3, MPI_COMM_WORLD);
     }
 
+    // Завершение MPI
     MPI_Finalize();
 
     return 0;
